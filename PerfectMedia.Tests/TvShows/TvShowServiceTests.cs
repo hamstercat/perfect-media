47a2f0bb4403bf531b504f8e11c86536eadb1c36
hamstercat <hamstercat@users.noreply.github.com>
2014-06-19 18:07:05 -0400

    Unit tests for TvShowService

---
+++ PerfectMedia.Tests/TvShows/TvShowServiceTests.cs
@@ -0,0 +1,120 @@
+﻿using NSubstitute;
+using PerfectMedia.TvShows;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PerfectMedia.Tests.TvShows
+{
+    public class TvShowServiceTests
+    {
+        private readonly IFileSystemService _fileSystemService;
+        private readonly TvShowService _service;
+
+        public TvShowServiceTests()
+        {
+            _fileSystemService = Substitute.For<IFileSystemService>();
+            _service = new TvShowService(_fileSystemService);
+        }
+
+        [Fact]
+        public void GetSeasons_WithFolders_ReturnsSeasons()
+        {
+            // Arrange
+            string path = @"C:\Folder";
+            List<string> seasonFolders = new List<string>
+            {
+                @"C:\Folder\Season 1",
+                @"C:\Folder\Season 2",
+                @"C:\Folder\Specials"
+            };
+            _fileSystemService.FindSeasonFolders(path)
+                .Returns(seasonFolders);
+
+            // Act
+            IEnumerable<Season> seasons = _service.GetSeasons(path);
+
+            // Assert
+            IEnumerable<string> actualSeasonFolders = seasons.Select(s => s.Path);
+            Assert.Equal(seasonFolders, actualSeasonFolders);
+        }
+
+        [Fact]
+        public void GetSeasons_WithoutFolders_ReturnsEmpty()
+        {
+            // Arrange
+            _fileSystemService.FindSeasonFolders(Arg.Any<string>())
+                .Returns(new List<string>());
+
+            // Act
+            IEnumerable<Season> seasons = _service.GetSeasons(@"C:\Folder");
+
+            // Assert
+            Assert.NotNull(seasons);
+            Assert.False(seasons.Any());
+        }
+
+        [Fact]
+        public void GetSeasons_EmptyPath_ThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                // Act
+                // Method returns an IEnumerable, enumerate it so the method executes
+                _service.GetSeasons(null).ToList();
+            });
+        }
+
+        [Fact]
+        public void GetEpisodes_WithFolders_ReturnsEpisodes()
+        {
+            // Arrange
+            string path = @"C:\Folder\Season 1";
+            List<string> episodeFolders = new List<string>
+            {
+                @"C:\Folder\Season 1\0x01.mkv",
+                @"C:\Folder\Season 1\0x02.mkv",
+                @"C:\Folder\Season 1\0x03.mkv"
+            };
+            _fileSystemService.FindVideoFiles(path)
+                .Returns(episodeFolders);
+
+            // Act
+            IEnumerable<Episode> episodes = _service.GetEpisodes(path);
+
+            // Assert
+            IEnumerable<string> actualEpisodeFolders = episodes.Select(s => s.Path);
+            Assert.Equal(episodeFolders, actualEpisodeFolders);
+        }
+
+        [Fact]
+        public void GetEpisodes_WithoutFolders_ReturnsEmpty()
+        {
+            // Arrange
+            _fileSystemService.FindVideoFiles(Arg.Any<string>())
+                .Returns(new List<string>());
+
+            // Act
+            IEnumerable<Episode> episodes = _service.GetEpisodes(@"C:\Folder");
+
+            // Assert
+            Assert.NotNull(episodes);
+            Assert.False(episodes.Any());
+        }
+
+        [Fact]
+        public void GetEpisodes_EmptyPath_ThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                // Act
+                // Method returns an IEnumerable, enumerate it so the method executes
+                _service.GetEpisodes(null).ToList();
+            });
+        }
+    }
+}

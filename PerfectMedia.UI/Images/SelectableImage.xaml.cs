73ce88d199f2c179549644cc7eb29a1500ec06e1
hamstercat <hamstercat@users.noreply.github.com>
2014-07-05 11:23:13 -0400

    UI for image selection

---
+++ PerfectMedia.UI/Images/SelectableImage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PerfectMedia.UI.Images
+{
+    /// <summary>
+    /// Interaction logic for SelectableImage.xaml
+    /// </summary>
+    public partial class SelectableImage : UserControl
+    {
+        public SelectableImage()
+        {
+            InitializeComponent();
+        }
+
+        private void GridMouseEnter(object sender, MouseEventArgs e)
+        {
+            ChangeMouseOverOverlayVisibility(Visibility.Visible);
+        }
+
+        private void GridMouseLeave(object sender, MouseEventArgs e)
+        {
+            ChangeMouseOverOverlayVisibility(Visibility.Hidden);
+        }
+
+        private void ChangeMouseOverOverlayVisibility(Visibility visibility)
+        {
+            MouseOverOverlay.Visibility = visibility;
+        }
+    }
+}

6867dd4835ea2862cbc4c16d21a9b8fb34d12609
hamstercat <hamstercat@users.noreply.github.com>
2014-09-21 12:27:27 -0400

    Reorder TV show metadata fields

--- PerfectMedia.UI/TvShows/Shows/TvGeneralInformation.xaml.cs
+++ PerfectMedia.UI/TvShows/Shows/TvGeneralInformation.xaml.cs
@@ -1,15 +1,24 @@
-﻿using System.Windows.Controls;
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace PerfectMedia.UI.TvShows.Shows
 {
     /// <summary>
     /// Interaction logic for TvGeneralInformation.xaml
     /// </summary>
-    public partial class TvGeneralInformation : UserControl
+    public partial class TvGeneralInformation
     {
         public TvGeneralInformation()
         {
             InitializeComponent();
+        }
+
+        private void PathHyperlinkClick(object sender, RoutedEventArgs e)
+        {
+            e.Handled = true;
+            string path = Path.Text;
+            Process.Start(path);
         }
     }
 }
